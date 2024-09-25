2d2eed62b496db77630bdb82bfea497dbe0b98f2
ThiTriOli <159439370+ThiTriOli@users.noreply.github.com>
2024-09-25 10:59:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ document.getElementById('btn-projetos').addEventListener('click', function() {
     const projetosSection = document.getElementById('projetos-section');
     projetosSection.scrollIntoView({ behavior: 'smooth' });
 
-    // Adiciona a classe "visible" após um pequeno atraso
+   
     setTimeout(() => {
         projetosSection.classList.add('visible');
-    }, 300); // Ajuste o tempo conforme necessário
+    }, 300); 
 });
