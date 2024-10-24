2d6622d5d441a6f54e4def72cf8e30b37eafed7e
ThiTriOli <159439370+ThiTriOli@users.noreply.github.com>
2024-10-24 08:20:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 document.getElementById('btn-projetos').addEventListener('click', function() {
+
     const projetosSection = document.getElementById('projetos-section');
     projetosSection.scrollIntoView({ behavior: 'smooth' });
-
    
     setTimeout(() => {
         projetosSection.classList.add('visible');
-    }, 300); 
+    }, 0); 
 });
