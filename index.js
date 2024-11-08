fba7659335947bca7d445215baf738155eec9117
ThiTriOli <159439370+ThiTriOli@users.noreply.github.com>
2024-11-08 20:51:15 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('btn-projetos').addEventListener('click', function() {
-
-    const projetosSection = document.getElementById('projetos-section');
-    projetosSection.scrollIntoView({ behavior: 'smooth' });
-   
-    setTimeout(() => {
-        projetosSection.classList.add('visible');
-    }, 0); 
-});
